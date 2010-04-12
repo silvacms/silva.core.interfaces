84aae1b6ac76551738836ba60971f0183eaa9d11
Sylvain Viollon <thefunny@gmail.com>
2010-04-12 09:38:19 +0000

    Rename base event, add role.

--- silva/core/interfaces/events.py
+++ silva/core/interfaces/events.py
@@ -3,24 +3,26 @@ from zope.interface import Attribute, implements
 from zope.component.interfaces import IObjectEvent, ObjectEvent
 
 
-class ISecurityChangedEvent(IObjectEvent):
+class ISecurityRoleChangedEvent(IObjectEvent):
 
     username = Attribute(u"Username affected by the change.")
+    role =  Attribute(u"Modified role.")
 
 
-class ISecurityRoleAddedEvent(ISecurityChangedEvent):
+class ISecurityRoleAddedEvent(ISecurityRoleChangedEvent):
     pass
 
-class ISecurityRoleRemovedEvent(ISecurityChangedEvent):
+class ISecurityRoleRemovedEvent(ISecurityRoleChangedEvent):
     pass
 
 
-class SecurityChangedEvent(ObjectEvent):
-    implements(ISecurityChangedEvent)
+class SecurityRoleChangedEvent(ObjectEvent):
+    implements(ISecurityRoleChangedEvent)
 
-    def __init__(self, obj, username):
+    def __init__(self, obj, username, role):
         super(SecurityChangedEvent, self).__init__(obj)
         self.username = username
+        self.role = role
 
 
 class SecurityRoleAddedEvent(SecurityChangedEvent):
