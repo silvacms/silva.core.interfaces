9c3900c71087c95a7ec5582f0ed49e5f313ec9dd
Sylvain Viollon <thefunny@gmail.com>
2010-04-28 11:56:08 +0000

    Add events.

--- silva/core/interfaces/events.py
+++ silva/core/interfaces/events.py
@@ -1,19 +1,75 @@
+# Copyright (c) 2009 Infrae. All rights reserved.
+# See also LICENSE.txt
+# $Id$
 
 from zope.interface import Attribute, implements
 from zope.component.interfaces import IObjectEvent, ObjectEvent
 
 
-class ISecurityRoleChangedEvent(IObjectEvent):
+# Content publication
 
+class IContentPublicationEvent(IObjectEvent):
+    pass
+
+
+class IContentApprovedEvent(IContentPublicationEvent):
+    pass
+
+
+class IContentClosedEvent(IContentPublicationEvent):
+    pass
+
+
+# Content import/export
+
+class IContentImportedExported(IObjectEvent):
+    pass
+
+
+class IContentImported(IContentImportedExported):
+    pass
+
+
+class IContentExported(IContentImportedExported):
+    pass
+
+
+# Security
+
+class ISecurityEvent(IObjectEvent):
+    """A security setting has been changed.
+    """
+
+
+class ISecurityRestrictionModifiedEvent(ISecurityEvent):
+    """A security restriction has been set on an object.
+    """
+    role = Attribute(u"Role needed to access the object")
+
+
+class ISecurityRoleChangedEvent(ISecurityEvent):
+    """A security role has been changed for a user.
+    """
     username = Attribute(u"Username affected by the change.")
-    role =  Attribute(u"Modified role.")
+    roles =  Attribute(u"Modified role.")
 
 
 class ISecurityRoleAddedEvent(ISecurityRoleChangedEvent):
-    pass
+    """A role as been attributed to a user.
+    """
+
 
 class ISecurityRoleRemovedEvent(ISecurityRoleChangedEvent):
-    pass
+    """A user as seen its access revoked.
+    """
+
+
+class SecurityRestrictionModifiedEvent(ObjectEvent):
+    implements(ISecurityRestrictionModifiedEvent)
+
+    def __init__(self, obj, role):
+        super(SecurityRestrictionModifiedEvent, self).__init__(obj)
+        self.role = role
 
 
 class SecurityRoleChangedEvent(ObjectEvent):
