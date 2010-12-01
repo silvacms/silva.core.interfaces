1ae3fe7c0c1454c91ac271c3d8c7a9ea7728f60e
Sylvain Viollon <thefunny@gmail.com>
2010-12-01 18:03:40 +0100

    Code removed, interface removed.

--- src/silva/core/interfaces/__init__.py
+++ src/silva/core/interfaces/__init__.py
@@ -5,20 +5,6 @@
 from zope.interface import Interface
 
 
-class IAccessManager(Interface):
-    """Mixin class for objects to request local roles on the object"""
-
-    def request_role(userid, role):
-        """Request a role on the current object and send an e-mail to the
-        editor/chiefeditor/manager"""
-
-    def allow_role(userid, role):
-        """Allows the role and send an e-mail to the user"""
-
-    def deny_role(userid, role):
-        """Denies the role and send an e-mail to the user"""
-
-
 class RequiredParameterNotSetError(Exception):
     pass
 
