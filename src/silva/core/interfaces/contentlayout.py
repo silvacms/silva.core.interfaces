4333f45cbabc8c7c6b2b1e2a4d3239918a0e2b17
Andy Altepeter <aaltepet@altepeter.net>
2011-02-28 15:23:47 -0600

    add default content template interface

--- src/silva/core/interfaces/contentlayout.py
+++ src/silva/core/interfaces/contentlayout.py
@@ -5,6 +5,25 @@ from zope.publisher.interfaces.browser import IBrowserRequest
 # for content layout (silva.core.contentlayout)
 #----------------------------------------------
 
+class IDefaultContentTemplate(Interface):
+    """The default content template for rendered Silva content which does not
+       support IContentLayout.  This view wraps the rendered silva content 
+       (passed in via set_content) with the desires default content template.
+       The dct can add a left nav (or not), a title (at the appropriate heading
+       level), etc.
+       
+       Override this in your own layer to provide a custom-tailored default
+       content template for your own layout.  You can also create layouts
+       tailored for specific content types by changing the grok.context.
+    """
+ 
+    def set_content(rendered_content):
+        """For a Default Content Template, the content has already been,
+           rendered.  Grok Views (of which DefaultContentTemplates are), do not
+           support passing in parameters to __call__.  Instead of overriding
+           __call__ (and breaking an implicit grok.View contract), we pass
+           in the content using this method before calling the object"""
+
 class IVersionedContentLayout(Interface):
     """Marker interface for VersionedContent objects wich
        store versiones supporting content layout
@@ -28,4 +47,5 @@ class IContentLayout(Interface):
            returns the list of parts in the specified slot
         """
 
-__all__ = ['IVersionedContentLayout','IContentLayout']
+__all__ = ['IVersionedContentLayout','IContentLayout', 
+           'IDefaultContentTemplate']
