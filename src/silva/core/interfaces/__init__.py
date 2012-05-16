90072c29deb600862678187e37f9c1f998e0020b
Sylvain Viollon <thefunny@gmail.com>
2012-05-16 14:21:59 +0200

    Port the mimetype classifier from the 2.3 branch.

--- src/silva/core/interfaces/__init__.py
+++ src/silva/core/interfaces/__init__.py
@@ -27,6 +27,38 @@ class IIcon(Interface):
         """
 
 
+class IMimeTypeClassifier(Interface):
+    """Detect content_type and extension for files.
+    """
+
+    def guess_filename(asset, basename):
+        """Given an ``IFile`` and a basename, set and return a correct
+        filename that would be usage for the file.
+        """
+
+    def guess_extension(content_type):
+        """Return the most plausible extension to use with a file of
+        the given type.
+        """
+
+    def guess_type(id=None, filename=None, buffer=None, default=None):
+        """Given a file id, or a filename, or a buffer containing some
+        data, return the most plausible content_type associated to a
+        file. If nothing is found and default is provided, return
+        default, otherwise 'application/octet-stream'.
+        """
+
+    def guess_buffer_type(buffer):
+        """Given a buffer containing some data, return the most
+        plausible content_type associated, or None.
+        """
+
+    def guess_file_type(filename):
+        """Given a filename pointing to a valid file on the disk,
+        return the most plausible content_type associated or None.
+        """
+
+
 class IUpgrader(Interface):
     """Interface for upgrade classes.
     """
